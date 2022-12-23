b811287cb3aaa451a6c6e49300f45e4f98ed2bab
Hassan Adnan <43107637+HassanAdnan123@users.noreply.github.com>
2022-12-23 19:04:18 +0500

    insertion and select queries

--- Connecting with Databases (JDBC)/src/main/java/Main.java
+++ Connecting with Databases (JDBC)/src/main/java/Main.java
@@ -1,5 +1,21 @@
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/computer-shop","root","abc123++");
 
+            Statement sql = con.createStatement();
+            sql.execute("insert into desktops (name, price, company) values ('A',1,'B')");
+            ResultSet views = sql.executeQuery("select * from desktops");
+            while(views.next())
+                System.out.println(views.getInt(1)+"\t"+views.getString(2)+"\t"+views.getString(3)+"\t"+views.getString(4));
+            con.close();
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
     }
 }
