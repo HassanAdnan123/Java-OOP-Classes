b6e2da79bc43051b95ad06e51c8fbca951080b46
Hassan Adnan <43107637+HassanAdnan123@users.noreply.github.com>
2022-12-24 13:08:34 +0500

    class updated content 24-12-22

---
+++ Connecting with Databases (JDBC)/db_backups/dump-computer-shop-202212231930.sql
@@ -0,0 +1,83 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: computer-shop
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `desktops`
+--
+
+DROP TABLE IF EXISTS `desktops`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `desktops` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `price` int DEFAULT NULL,
+  `company` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `desktops`
+--
+
+LOCK TABLES `desktops` WRITE;
+/*!40000 ALTER TABLE `desktops` DISABLE KEYS */;
+INSERT INTO `desktops` VALUES (1,'Dell Optiplex 7010',200,'Dell'),(2,'Lenovo Thinkcenter Z400',450,'Lenovo'),(5,'A',1,'B'),(6,'A',1,'B'),(7,'A',1,'B');
+/*!40000 ALTER TABLE `desktops` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `laptops`
+--
+
+DROP TABLE IF EXISTS `laptops`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `laptops` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `price` int DEFAULT NULL,
+  `company` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `laptops`
+--
+
+LOCK TABLES `laptops` WRITE;
+/*!40000 ALTER TABLE `laptops` DISABLE KEYS */;
+INSERT INTO `laptops` VALUES (1,'Lenovo Thinkpad T470',500,'Lenovo'),(2,'Macbook Pro',1200,'Apple');
+/*!40000 ALTER TABLE `laptops` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'computer-shop'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-23 19:30:46
--- Connecting with Databases (JDBC)/src/main/java/Main.java
+++ Connecting with Databases (JDBC)/src/main/java/Main.java
@@ -1,15 +1,35 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         try{
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Computer Model: ");
+        String model = sc.nextLine();
+        System.out.println("Price: ");
+        String price = sc.nextLine();
+        System.out.println("Manufacturer Company: ");
+        String company = sc.nextLine();
+
+
             Class.forName("com.mysql.jdbc.Driver");
             Connection con=DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/computer-shop","root","abc123++");
+                    "jdbc:mysql://localhost:3306/computershop?autoReconnect=true&useSSL=false","root","abc123++");
+             //                                          ^^database name
 
             Statement sql = con.createStatement();
-            sql.execute("insert into desktops (name, price, company) values ('A',1,'B')");
+
+            // Insert Query Syntax
+            sql.execute(String.format("insert into desktops (name, price, company) values ('%s',%s,'%s')",model,price,company));
+            // INSERT INTO DESKTOPS (name, price, company) VALUES ('compaq',980, 'acer')
+
+
+            // Select Query Syntax
             ResultSet views = sql.executeQuery("select * from desktops");
+
             while(views.next())
                 System.out.println(views.getInt(1)+"\t"+views.getString(2)+"\t"+views.getString(3)+"\t"+views.getString(4));
             con.close();
